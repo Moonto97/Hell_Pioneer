795c235c09b27ba58ce303b877f94f2857e2123c
hmssojeong <sojeong9638@gmail.com>
2025-11-21 15:43:42 +0900

    feat: 플레이어 에셋 및 모션 적용
    대각선은 아직 이동 불가

--- Assets/02.Scripts/PlayerMove.cs
+++ Assets/02.Scripts/PlayerMove.cs
@@ -4,30 +4,89 @@ public class PlayerMove : MonoBehaviour
 {
 
     private float _speed = 3f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+
+    private Rigidbody2D _rb;
+    private Vector2 _playerMove;
+    private Animator _animator;
+
+
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>(); 
+      
     }
 
-    // Update is called once per frame
+    public void Execute()
+    {
+
+    }
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
+        Vector2 moveInput = new Vector2(h, v);
 
-        Vector2 direction = new Vector2(h, v);
-        direction.Normalize();
 
-        Vector3 movement = new Vector3(h, v, 0f);
-        transform.Translate(movement * _speed * Time.deltaTime);
+        float moveX = 0f;
+        float moveY = 0f;
 
-        MakePlayerDistanceField();
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = 1f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+
+        // 대각선 이동 방지
+        if (moveX != 0)
+        {
+            moveY = 0;
+        }
+
+        bool isMoving = moveX != 0 || moveY != 0;
+
+        _animator.SetFloat("MoveX", moveX);
+        _animator.SetFloat("MoveY", moveY);
+        _animator.SetBool("isMoving", isMoving);
+
+        _playerMove = new Vector2(moveX, moveY);
+        transform.Translate(_playerMove * _speed * Time.deltaTime);
+
+
+        /*        float h = Input.GetAxisRaw("Horizontal");
+                float v = Input.GetAxisRaw("Vertical");
+
+                Vector2 direction = new Vector2(h, v);
+                direction.Normalize();*/
+
+        /*        Vector3 movement = new Vector3(h, v, 0f);
+                transform.Translate(movement * _speed * Time.deltaTime);
+
+                MakePlayerDistanceField();*/
     }
-    
+
     // TODO : 움직일때, 방해물 생성시에만 호출
     void MakePlayerDistanceField()
     {
         BFSManager.Instance.BuildDistanceField(transform.position);
     }
+
+/*    private void FixedUpdate()
+    {
+        _rb.linearVelocity = new Vector2(0, v) * _speed * Time.deltaTime;
+    }*/
+
 }
