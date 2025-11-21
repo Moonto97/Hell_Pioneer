96fc8688aac822824504fac99ba7d8b591f029ae
hollo112 <76999794+hollo112@users.noreply.github.com>
2025-11-21 19:04:39 +0900

    feat: 레이저몬스터 오브젝트풀링 추가

---
+++ Assets/02.Scripts/Manager/Monster/TEST.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TEST : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MakePlayerDistanceField();
+    }
+    void MakePlayerDistanceField()
+    {
+        BFSManager.Instance.BuildDistanceField(transform.position);
+    }
+}
